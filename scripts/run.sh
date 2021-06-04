7b84af451e1fe60b7dcdd1c8d9a0b5a188c1f325
Eunsuk Kang <eunsuk.kang@gmail.com>
2021-06-04 10:56:54 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-echo "Run Course Scheduling problems"
+echo "Running all benchmark problems"
+echo "Running Course Scheduling problems..."
 cd course
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
 python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "Course Scheduling problems complete"
 
-echo "Run CheckMate problems"
+echo "Runnning CheckMate problems..."
 cd ../checkmate
 python benchmark.py -maxsat -t=1800 -r=1 > one_result_maxsat.csv
 python benchmark.py -maxsat_part -t=1800 -r=1 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 > one_result_maxsat_part_auto.csv
 echo "CheckMate problems complete"
 
-echo "Run Seating problems"
+echo "Running Seating problems..."
 cd ../seating
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
 python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "Seating problems complete"
 
-echo "Run SMS problems"
+echo "Runnning SMS problems..."
 cd ../SMS
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
 python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "SMS problems complete"
 
-echo "Run Graceful Degradation problems"
+echo "Runnning Graceful Degradation problems..."
 cd ../degradation
 python benchmark.py -maxsat -t=1800 -r=1 -from_file > one_result_maxsat.csv
 python benchmark.py -maxsat_part -t=1800 -r=1 -from_file > one_result_maxsat_part.csv
