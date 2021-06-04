825576139d9aa8d48017cf04cbdfbc240e62d1e6
Changjian Zhang <mljzcj@gmail.com>
2021-06-04 09:40:05 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,30 +3,35 @@
 echo "Run Course Scheduling problems"
 cd course
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
+python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "Course Scheduling problems complete"
 
 echo "Run CheckMate problems"
 cd ../checkmate
 python benchmark.py -maxsat -t=1800 -r=1 > one_result_maxsat.csv
+python benchmark.py -maxsat_part -t=1800 -r=1 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 > one_result_maxsat_part_auto.csv
 echo "CheckMate problems complete"
 
 echo "Run Seating problems"
 cd ../seating
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
+python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "Seating problems complete"
 
 echo "Run SMS problems"
 cd ../SMS
 python benchmark.py -maxsat -t=1800 -r=1 -m=models_20210203 > one_result_maxsat.csv
+python benchmark.py -maxsat_part -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -m=models_20210203 > one_result_maxsat_part_auto.csv
 echo "SMS problems complete"
 
 echo "Run Graceful Degradation problems"
 cd ../degradation
 python benchmark.py -maxsat -t=1800 -r=1 -from_file > one_result_maxsat.csv
+python benchmark.py -maxsat_part -t=1800 -r=1 -from_file > one_result_maxsat_part.csv
 python benchmark.py -maxsat_part_auto -t=1800 -r=1 -from_file > one_result_maxsat_part_auto.csv
 echo "Graceful Degradation problems compelte"
 
